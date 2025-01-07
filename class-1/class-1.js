b2eb42f16103b495f8bc772bbd6b8474f40a4e26
sourav-gdrf <chakrabartty93@gmail.com>
2025-01-07 17:47:32 +0600

    Create class-1.js

---
+++ class-1/class-1.js
@@ -0,0 +1,27 @@
+var a = 1;
+var b = "hello";
+var c = false;
+console.log(typeof c)
+var d = null;
+var e = undefined;
+var f = {
+  name: "sourav", 
+  age: 24,
+  married: false, 
+  children: ["child 1","child 2","child 3"],
+  car: null
+};
+console.log(f)
+var g = [ "a","b","c"];//array
+var h;
+console.log(typeof a)
+console.log(typeof b)
+console.log(typeof c)
+console.log(typeof d)
+console.log(typeof e)
+console.log(typeof f)
+console.log(typeof g)
+console.log( g)
+console.log( g[0])
+console.log( g[1])
+console.log(typeof h)
